2b5124916198c180e6ff8fbc672cd45b0cf3997a
pablodev <pabloteixeira591@gmail.com>
2025-01-31 23:08:53 -0300

    update 1.0 - Adding all content off home page - Addind animations file css - addind all structure of web site

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./ui/components/Header";
+import { motion } from "motion/react"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Header />
 
-export default App
+      <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}  transition={{ duration: 1 }} className="flex flex-col items-center justify-center h-screen space-y-4">
+        
+        <h1 className="text-6xl font-bold text-white glow mb-4">Athon Networks</h1>
+        <p>
+          Cloud computing, network services, technological solutions that
+          together helping your company be part of the future...{" "}
+        </p>
+      
+        <div className="flex justify-center items-center gap-12">
+        <div className="float-icon">
+          <img src="./cloud.png" alt="" />
+        </div>
+        <div className="float-icon">
+        <img src="./security.png" alt="" />
+        </div>
+        <div className="float-icon">
+        <img src="./support.png" alt="" />
+
+        </div>
+        <div className="float-icon">
+        <img src="./code.png" alt="" />
+
+        </div>
+      </div>
+      </motion.div>
+      <div id="services" className="h-screen">
+      </div>
+    </div>
+  );
+}
---
+++ src/ui/components/Header.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export default function Header() {
+
+  const [isAnimating, setIsAnimating] = useState(false);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setIsAnimating(true);
+        setTimeout(() => setIsAnimating(false), 4000);
+      }, 5000);
+  
+      return () => clearInterval(interval);
+    }, []);
+
+  return (
+    <header className="flex justify-between items-center p-4 px-40">
+      <div className="flex items-center gap-2">
+        <span className={`letter ${isAnimating ? 'animate' : ''}`}>A</span>
+        <span className={`letter ${isAnimating ? 'animate' : ''}`}>T</span>
+        <span className={`letter ${isAnimating ? 'animate' : ''}`}>H</span>
+        <img width={30} src="./athonfav.png" alt="" className={`logo ${isAnimating ? 'animate' : ''}`} />
+        <span className={`letter ${isAnimating ? 'animate' : ''}`}>N</span>
+      </div>
+
+      <nav>
+        <ul className="flex gap-6 items-center">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <a href="services">
+        <button>Services</button>
+      </a>
+    </header>
+  );
+}
