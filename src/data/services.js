efecaffd588e7dd4c924e96228b5c15626c37e94
pablodev <pabloteixeira591@gmail.com>
2025-02-10 20:31:59 -0300

    adding all content of modals service, add new durations on the transitions when I click in any button

--- src/data/services.js
+++ src/data/services.js
@@ -1,53 +1,130 @@
 const services = [
-    {
-      src: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "Athon Docs",
-      description: "Serviço exclusivo da Athon Networks, criado para oferecer uma solução robusta e prática na documentação de redes e infraestruturas empresariais.",
-      link: "#",
-      linkMessage: "Clique para saber mais",
-      more: "Digite todo o texto para explicar direito sobre o projeto",
-    },
-    {
-      src: "https://plus.unsplash.com/premium_photo-1663040454423-30ce7ed209ad?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "Athon EYE",
-      description: "Serviço inovador da Athon Networks voltado para o monitoramento eficiente e em tempo real de ambientes empresariais.",
-      link: "#",
-      linkMessage: "Clique para saber mais",
-      more: "Digite todo o texto para explicar direito sobre o projeto",
-    },
-    
-    {
-        src: "https://images.unsplash.com/photo-1516116216624-53e697fedbea?auto=format&fit=crop&w=400&h=300",
-        title: "Athon Engineering",
-        description: "Formado por especialistas altamente qualificados em roteamento de redes e na aplicação de protocolos avançados.",
-        link: "#",
-        linkMessage: "Clique para saber mais",
-        more: "Digite todo o texto para explicar direito sobre o projeto",
-      },
-      {
-        src: "https://plus.unsplash.com/premium_photo-1682145177356-33efa0ee06f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        title: "Athon Cloud",
-        description: "Especializado em implementar soluções em nuvem utilizando as principais plataformas do mercado: AWS, Oracle Cloud e Google Cloud.",
-        link: "#",
-        linkMessage: "Clique para saber mais",
-        more: "Digite todo o texto para explicar direito sobre o projeto",
-      },
-      {
-        src: "https://images.unsplash.com/photo-1614064642261-3ccbfafa481b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        title: "Athon CiberSec",
-        description: "Serviço de cibersegurança desenvolvido para proteger a infraestrutura digital de sua empresa contra ameaças crescentes no ambiente online.",
-        link: "#",
-        linkMessage: "Clique para saber mais",
-        more: "Digite todo o texto para explicar direito sobre o projeto",
-      },
-      {
-        src: "https://images.unsplash.com/photo-1588600878108-578307a3cc9d?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        title: "Athon Supervisor",
-        description: "Solução completa da Athon Networks para o monitoramento e a gestão do desempenho de redes, servidores e aplicações.",
-        link: "#",
-        linkMessage: "Clique para saber mais",
-        more: "Digite todo o texto para explicar direito sobre o projeto",
-      },
-  ];
+  {
+    src: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Athon Docs",
+    description:
+      "Serviço exclusivo da Athon Networks, criado para oferecer uma solução robusta e prática na documentação de redes e infraestruturas empresariais.",
+    link: "#",
+    linkMessage: "Clique para saber mais",
+    services: [
+      "Mapeamento Inteligente da Infraestrutura",
+      "Gestão Centralizada de Dados",
+      "Automação e Atualização Contínua",
+      "Protocolos e Boas Práticas de Segurança",
+    ],
+    beneficios: [
+      "Melhoria na agilidade e eficiência da gestão de redes",
+      "Facilidade no treinamento e integração de equipes de TI.",
+      "Redução de riscos operacionais e compliance com normas regulatórias.",
+      "Visão clara e controle total do ambiente tecnológico da empresa.",
+    ],
+  },
+  {
+    src: "https://plus.unsplash.com/premium_photo-1663040454423-30ce7ed209ad?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Athon EYE",
+    description:
+      "Serviço inovador da Athon Networks voltado para o monitoramento eficiente e em tempo real de ambientes empresariais.",
+    link: "#",
+    linkMessage: "Clique para saber mais",
+    services: [
+      "Monitoramento em Tempo Real",
+      "Soluções Sob Medida",
+      "Alertas Inteligentes e Automatizados",
+      "Armazenamento Seguro e Acessível",
+      "Acesso Remoto e Controle Total",
+    ],
+    beneficios: [
+      "Aumento da segurança e redução de vulnerabilidades.",
+      "Controle contínuo sobre áreas críticas do negócio.",
+      "Respostas rápidas e eficazes a incidentes.",
+      "Simplicidade na gestão de um sistema avançado de monitoramento.",
+    ],
+  },
 
-export default services
+  {
+    src: "https://images.unsplash.com/photo-1516116216624-53e697fedbea?auto=format&fit=crop&w=400&h=300",
+    title: "Athon Engineering",
+    description:
+      "Formado por especialistas altamente qualificados em roteamento de redes e na aplicação de protocolos avançados.",
+    link: "#",
+    linkMessage: "Clique para saber mais",
+    services: [
+      "Domínio Avançado de Protocolos de Rede",
+      "Arquiteturas Escaláveis e de Alto Desempenho",
+      "Diagnóstico Preciso e Otimização de Redes",
+      "Consultoria Estratégica para Infraestrutura",
+      "Inovação Contínua e Atualização Tecnológica",
+    ],
+    beneficios: [
+      "Excelência Técnica: Profissionais certificados e com ampla experiência no mercado.",
+      "Abordagem Personalizada: Cada projeto é tratado como único, garantindo soluções sob medida.",
+      "Foco em Resultado: Redes mais rápidas, confiáveis e alinhadas aos objetivos do cliente.",
+      "Base de Suporte Interna: Disponível para atender os serviços da Athon Networks e contribuir para o sucesso de nossos clientes.",
+    ],
+  },
+  {
+    src: "https://plus.unsplash.com/premium_photo-1682145177356-33efa0ee06f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Athon Cloud",
+    description:
+      "Especializado em implementar soluções em nuvem utilizando as principais plataformas do mercado: AWS, Oracle Cloud e Google Cloud.",
+    link: "#",
+    linkMessage: "Clique para saber mais",
+    services: [
+      "Estratégia Inteligente de Migração",
+      "Infraestrutura Multi-Cloud Sob Medida",
+      "Eficiência Operacional e Redução de Custos",
+      "Segurança e Compliance Avançados",
+      "Arquitetura Escalável e Alta Disponibilidade",
+    ],
+    beneficios: [
+      "Agilidade e flexibilidade no gerenciamento de recursos de TI.",
+      "Redução de custos com infraestrutura física.",
+      "Maior resiliência e recuperação rápida de desastres.",
+      "Capacidade de inovação acelerada com acesso a serviços avançados.",
+    ],
+  },
+  {
+    src: "https://images.unsplash.com/photo-1614064642261-3ccbfafa481b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Athon CiberSec",
+    description:
+      "Serviço de cibersegurança desenvolvido para proteger a infraestrutura digital de sua empresa contra ameaças crescentes no ambiente online.",
+    link: "#",
+    linkMessage: "Clique para saber mais",
+    services: [
+      "Análise Profunda de Vulnerabilidades",
+      "Defesa Avançada Contra Ameaças",
+      "Consultoria Estratégica em Segurança",
+      "Resposta Eficiente a Incidentes",
+      "Conformidade e Segurança Regulatória",
+    ],
+    beneficios: [
+      "Prevenção contra perdas financeiras e danos à reputação.",
+      "Maior tranquilidade para focar no crescimento do negócio.",
+      "Conformidade com regulamentações que fortalecem a confiança de clientes e parceiros.",
+      "Acesso a expertise técnica de alto nível para lidar com desafios complexos.​",
+    ],
+  },
+  {
+    src: "https://images.unsplash.com/photo-1588600878108-578307a3cc9d?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Athon Supervisor",
+    description:
+      "Solução completa da Athon Networks para o monitoramento e a gestão do desempenho de redes, servidores e aplicações.",
+    link: "#",
+    linkMessage: "Clique para saber mais",
+    services: [
+      "Monitoramento Inteligente e Centralizado",
+      "Análises e Dashboards Avançados",
+      "Alertas Automatizados e Proativos",
+      "Relatórios Estratégicos e Insights",
+      "Customização e Integração Flexível",
+    ],
+    beneficios: [
+      "Redução de interrupções e maior disponibilidade dos serviços.",
+      "Identificação rápida de gargalos e problemas potenciais.",
+      "Planejamento estratégico com base em dados confiáveis e consistentes.",
+      "Maior eficiência operacional e tranquilidade para sua equipe de TI.​",
+    ],
+  },
+];
+
+export default services;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import CardServices from "../ui/components/CardServices";
 import services from "../data/services";
+import { li } from "motion/react-client";
 
 export default function Services() {
   const [activeModal, setActiveModal] = useState(null);
@@ -35,12 +36,39 @@ export default function Services() {
 
               {activeModal === index && (
                 <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50">
-                  <div className="bg-zinc-900 p-6 rounded-lg max-w-md relative">
-                    <h3 className="text-xl font-bold">{service.title}</h3>
-                    <p className="mt-2">{service.description}</p>
+                  <div className="bg-black p-6 rounded-lg relative">
+                    <h3 className="text-3xl font-bold">{service.title}</h3>
+                    <p className="mt-2 max-w-6/10 mb-10">
+                      {service.description}
+                    </p>
+                    <div className="space-y-4">
+                      <h4 className="text-xl mb-5">Serviços</h4>
+                      <ul className="list-disc">
+                        {service.services.map((service, index) => (
+                          <li className="lis" key={index}>
+                            {service}
+                          </li>
+                        ))}
+                      </ul>
+                      <h4 className="text-xl mb-5">Beneficios</h4>
+                      <ul className="list-disc">
+                        {service.beneficios.map((beneficio, index) => (
+                          <li className="lis" key={index}>
+                            {beneficio}
+                          </li>
+                        ))}
+                      </ul>
+                      <a className="text-green-400" href="">
+                        <button className="cursor-pointer">
+                          Estou interessado, gostaria de conversar com algum
+                          representante.
+                        </button>
+                      </a>
+                    </div>
+
                     <button
                       onClick={closeModal}
-                      className="mt-4 bg-white text-black px-4 py-2 rounded"
+                      className="mt-4 bg-white text-black px-4 py-2 rounded cursor-pointer hover:bg-black hover:text-white border border-white rounded-8 duration-700"
                     >
                       Fechar
                     </button>
